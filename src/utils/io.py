b7153a99aabbe4894cca34e1486103a31d1fe62f
Monkin <monkinsmurf@gmail.com>
2024-05-16 20:59:08 +0100

    Update: utils to include gif utility

--- src/utils/io.py
+++ src/utils/io.py
@@ -1,4 +1,5 @@
-from numpy import set_printoptions
+from numpy import set_printoptions, uint8, max
+from PIL import Image
 
 def preview_np_array(np_array, array_label="Np array", edge_items=5):
     """
@@ -9,3 +10,22 @@ def preview_np_array(np_array, array_label="Np array", edge_items=5):
     """
     set_printoptions(threshold=edge_items*2, edgeitems=edge_items)
     print(f"{array_label} Shape: {np_array.shape}.\nPreview: {np_array}")
+
+def make_gif(img_arr, filepath, duration=100, loop=0):
+    """
+    Makes a gif from an array of images
+    @img_arr: The array of images to make the gif from
+    @filepath: The path to save the gif
+    @duration: The duration of each frame in milliseconds (default 100)
+    @loop: The number of times to loop the gif (default 1)
+    """
+    if max(img_arr) <= 1:
+        # Convert to 0-255 scale
+        img_arr = img_arr * 255
+
+    frames = [Image.fromarray(uint8(img)) for img in img_arr]
+    frame_one = frames[0]
+
+    frame_one.save(filepath, format="GIF", append_images=frames,
+               save_all=True, duration=duration, loop=loop, )
+    
