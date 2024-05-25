1b2c256b417e364e526d70a8ba9fd636d93a3590
Monkin <monkinsmurf@gmail.com>
2024-05-25 19:17:46 +0100

    Add: Extracting the spikes from each feature neuron that correspond to HFOs

--- src/utils/snn.py
+++ src/utils/snn.py
@@ -1,3 +1,5 @@
+from .input import MarkerType
+
 init_offset = 3400  # 33400      #   
 virtual_time_step_interval = 1  # TODO: Check if this should be the time-step value. it is not aligned with the sampling rate of the input data
 
@@ -18,3 +20,38 @@ class SNNSimConfig:
         self.init_offset = init_offset
         self.virtual_time_step_interval = virtual_time_step_interval
         self.num_steps = num_steps
+
+    def real_time_to_iter(self, real_time):
+        """
+        This function converts the real time to the iteration in the simulation.
+        @param real_time (int): The real time to convert to the iteration.
+
+        @return (int): The iteration in the simulation.
+        """
+        if real_time < self.init_offset:
+            # If the real time is before the simulation starts, return index 0
+            return 0
+        
+        return (real_time - self.init_offset) // self.virtual_time_step_interval
+
+class NeuronClass:
+    """Enum for the different classes of Feature Neurons"""
+    SILENT = 0
+    NOISY = 1
+    RIPPLE_DETECTOR = 2
+    FR_DETECTOR = 3
+    BOTH = 4
+
+def marker_type_to_neuron_class(marker_type: MarkerType) -> NeuronClass:
+    """
+    This function returns the neuron class for the marker type.
+    If the marker type is unknown, it returns the noisy class.
+    """
+    if marker_type == MarkerType.RIPPLE:
+        return NeuronClass.RIPPLE_DETECTOR
+    elif marker_type == MarkerType.FAST_RIPPLE:
+        return NeuronClass.FR_DETECTOR
+    elif marker_type == MarkerType.BOTH:
+        return NeuronClass.BOTH
+    else:
+        return NeuronClass.NOISY
