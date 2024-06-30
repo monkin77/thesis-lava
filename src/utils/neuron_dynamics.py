8149e48669f5a2e131fe038436c4e6fe8746872c
Monkin <monkinsmurf@gmail.com>
2024-06-30 02:27:56 +0100

    Add: updating results

--- src/utils/neuron_dynamics.py
+++ src/utils/neuron_dynamics.py
@@ -4,3 +4,8 @@ def time_constant_to_fraction(tau):
     """Calculate the (du or dv) value for a given time constant tau."""
     fraction_val = 1 - np.exp(-1/tau)
     return fraction_val
+
+def fraction_to_time_constant(fraction):
+    """Calculate the time constant tau for a given fraction value."""
+    tau_val = -1/np.log(1-fraction)
+    return tau_val
